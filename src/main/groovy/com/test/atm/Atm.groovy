cc32bfd93120137a7c55d3937b187498e0d8bd0f
Alexey Udalykh <udalykh@newstandards-us.com>
2016-11-25 18:31:37 +0800

    Initial commit

---
+++ src/main/groovy/com/test/atm/Atm.groovy
@@ -0,0 +1,20 @@
+package com.test.atm
+
+
+class Atm {
+
+    private static final String QUIT_WORD = "QUIT"
+    private static final String OK_WORD = "OK"
+    private static final String ERROR_WORD = "ERROR"
+
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in)
+
+        Atm atm = new Atm()
+        while (true) {
+            // pass parameters from console to ATM
+            // TODO
+        }
+    }
+}
