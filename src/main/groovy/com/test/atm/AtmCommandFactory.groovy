e21cb663681b85fd124243dd4e56fbd9e49a0f81
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-29 22:22:44 +0800

    Groovy refactoring

---
+++ src/main/groovy/com/test/atm/AtmCommandFactory.groovy
@@ -0,0 +1,22 @@
+package com.test.atm
+
+class AtmCommandFactory {
+    private MoneyStorage moneyStorage
+
+    AtmCommandFactory(MoneyStorage moneyStorage) {
+        this.moneyStorage = moneyStorage
+    }
+
+    AtmCommand create(String action) throws AtmStateException {
+        switch (CommandType.getCommandType(action)) {
+            case CommandType.REMAININGS:
+                return new RequestRemainings(moneyStorage)
+            case CommandType.ADD:
+                return new DepositCommand(moneyStorage)
+            case CommandType.WITHDRAW:
+                return new WithdrawalCommand(moneyStorage)
+            default:
+                throw new AtmStateException('Cannot find a command for this action ' + action)
+        }
+    }
+}
