0e7c4208be456ba0d467d81a617e75ae4164f2b7
DUdalykh <dmitry.udalykh@yandex.ru>
2016-11-29 23:20:41 +0800

    Groovy refactoring

---
+++ src/main/groovy/com/test/atm/CommandType.groovy
@@ -0,0 +1,22 @@
+package com.test.atm
+
+enum CommandType {
+    REMAININGS("?"),
+    ADD("+"),
+    WITHDRAW("-");
+
+    private final String command;
+
+    CommandType(String command) {
+        this.command = command;
+    }
+
+    public static CommandType getCommandType(String command) throws AtmStateException {
+        for (def commandTypeCheck : values()) {
+            if (commandTypeCheck.toString() == command) {
+                return commandTypeCheck
+            }
+        }
+        throw new AtmStateException('UNIDENTIFIED COMMAND')
+    }
+}
