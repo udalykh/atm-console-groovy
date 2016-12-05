6b19f73cef8d9c69487bc9afff589c3c87647947
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-05 19:42:54 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/BankNote.groovy
+++ src/main/groovy/com/test/atm/BankNote.groovy
@@ -13,6 +13,7 @@ class BankNote {
         currency = thisCurrency
         value = thisValue
     }
+
     boolean equals(Object o) {
         if (this == o) return true
         if (o == null || getClass() != o.getClass()) return false
@@ -20,6 +21,7 @@ class BankNote {
         if (value != bankNoteCheck.value) return false
         return currency != null ? currency == bankNoteCheck.currency : bankNoteCheck.currency != null
     }
+
     int hashCode() {
         int result = currency ? currency.hashCode() : 0
         result = 128 * result + value
