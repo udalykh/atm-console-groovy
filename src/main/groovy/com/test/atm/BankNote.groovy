62d4952430fcf1404b173cf62f84622c83afd592
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-05 19:20:37 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/BankNote.groovy
+++ src/main/groovy/com/test/atm/BankNote.groovy
@@ -3,7 +3,7 @@ package com.test.atm
 import groovy.transform.EqualsAndHashCode
 import groovy.transform.Sortable
 
-// Good job with @Sortable!
+@EqualsAndHashCode
 @Sortable(includes = 'currency, value')
 class BankNote {
     final Currency currency
@@ -13,9 +13,6 @@ class BankNote {
         currency = thisCurrency
         value = thisValue
     }
-
-    // TODO: replace with @EqualsAndHashCode annotation
-    @Override
     boolean equals(Object o) {
         if (this == o) return true
         if (o == null || getClass() != o.getClass()) return false
@@ -23,23 +20,10 @@ class BankNote {
         if (value != bankNoteCheck.value) return false
         return currency != null ? currency == bankNoteCheck.currency : bankNoteCheck.currency != null
     }
-
-    // TODO: replace with @EqualsAndHashCode annotation
-    @Override
     int hashCode() {
         int result = currency ? currency.hashCode() : 0
         result = 128 * result + value
         return result
-    }
-
-    //TODO: not needed Groovy will create a getter under the hood
-    Currency getCurrency() {
-        return currency
-    }
-
-    //TODO: not needed Groovy will create a getter under the hood
-    int getValue() {
-        return value
     }
 
     @Override
