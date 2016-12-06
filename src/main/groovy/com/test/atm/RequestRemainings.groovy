354a3c60c0f87ada143c7031cd38299a41f2b1fc
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-06 23:29:41 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/RequestRemainings.groovy
+++ src/main/groovy/com/test/atm/RequestRemainings.groovy
@@ -8,6 +8,6 @@ class RequestRemainings implements AtmCommand {
     }
 
     Map<BankNote, Integer> execute(String... arguments) {
-        return moneyStorage.getBanknotes()
+        return moneyStorage.notes
     }
 }
