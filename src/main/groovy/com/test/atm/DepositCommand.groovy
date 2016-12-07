db0258b709de45cbb0e92e08dbde3587fffc85fd
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-07 11:33:48 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/DepositCommand.groovy
+++ src/main/groovy/com/test/atm/DepositCommand.groovy
@@ -7,7 +7,7 @@ class DepositCommand implements AtmCommand {
         this.moneyStorage = moneyStorage
     }
 
-    Map<BankNote, Integer> execute(String... arguments) throws AtmStateException {
+    Map<BankNote, Integer> execute(String... arguments) {
         AtmUtils.assertLengthCheck(3, arguments)
         Currency currencyToPut = Currency.getCurrency(arguments[0])
         int valueToPut = AtmUtils.parseInt(arguments[1], 'ILLEGAL TYPING OF VALUE')
