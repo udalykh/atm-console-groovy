99dc2f602566d44a5921b931c1d94ec0c1518494
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-07 19:01:52 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/CommandTypeTest.groovy
+++ src/main/groovy/com/test/atm/CommandTypeTest.groovy
@@ -5,15 +5,14 @@ package com.test.atm
  */
 class CommandTypeTest extends GroovyTestCase{
 
-    @Test
     public void testAtmCommand() throws AtmStateException {
         CommandType addCheck = CommandType.getCommandType("+")
-        Assert.assertEquals(addCheck, CommandType.ADD)
+        assertEquals(addCheck, CommandType.ADD)
 
         CommandType withdrawalCheck = CommandType.getCommandType("-")
-        Assert.assertEquals(withdrawalCheck, CommandType.WITHDRAW)
+        assertEquals(withdrawalCheck, CommandType.WITHDRAW)
 
         CommandType remainCheck = CommandType.getCommandType("?")
-        Assert.assertEquals(remainCheck, CommandType.REMAININGS)
+        assertEquals(remainCheck, CommandType.REMAININGS)
     }
 }
