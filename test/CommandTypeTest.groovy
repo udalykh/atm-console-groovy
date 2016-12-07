a088827eab62cc39ea8e25ffceaf7dbd8d124de5
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-07 18:51:55 +0800

    Groovy refactoring

---
+++ test/CommandTypeTest.groovy
@@ -0,0 +1,21 @@
+package com.test.atm
+
+/**
+ * Created by Test on 12/07/2016.
+ */
+import org.junit.Test
+
+class CommandTypeTest {
+
+    @Test
+    public void testAtmCommand() throws AtmStateException {
+        CommandType addCheck = CommandType.getCommandType("+");
+        Assert.assertEquals(addCheck, CommandType.ADD);
+
+        CommandType withdrawalCheck = CommandType.getCommandType("-");
+        Assert.assertEquals(withdrawalCheck, CommandType.WITHDRAW);
+
+        CommandType remainCheck = CommandType.getCommandType("?");
+        Assert.assertEquals(remainCheck, CommandType.REMAININGS);
+    }
+}
