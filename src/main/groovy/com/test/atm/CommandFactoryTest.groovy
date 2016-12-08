339b19e9d93c6d5bbf105b94d75ae75ebb05577e
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-08 12:29:39 +0800

    Groovy refactoring

---
+++ src/main/groovy/com/test/atm/CommandFactoryTest.groovy
@@ -0,0 +1,19 @@
+package com.test.atm
+
+/**
+ * Created by Test on 12/08/2016.
+ */
+class CommandFactoryTest extends GroovyTestCase {
+    private MoneyStorage moneyStorage = new MoneyStorage()
+    private AtmCommandFactory atmCommandFactory
+
+    void setUp() {
+        atmCommandFactory = new AtmCommandFactory(moneyStorage)
+    }
+
+    void defaultAtmCommand() {
+        shouldFail(AtmStateException) {
+            atmCommandFactory.create('ABC')
+        }
+    }
+}
