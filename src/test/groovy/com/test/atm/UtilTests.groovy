9c1ed10ca1f06a65fdb8b1c2a7c1b9d37b322f62
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-08 22:26:45 +0800

    Groovy refactoring

---
+++ src/test/groovy/com/test/atm/UtilTests.groovy
@@ -0,0 +1,20 @@
+package com.test.atm
+
+/**
+ * Created by Test on 12/08/2016.
+ */
+class UtilTests extends GroovyTestCase{
+    void utilTestOne() {
+        shouldFail(AtmStateException) {
+            AtmUtils.parseInt('ABC', 'STRING IS NOT PARSABLE')
+        }
+    }
+
+    void utilTestTwo() {
+        shouldFail(AtmStateException) {
+            String[] stringArray = ['One', 'Two']
+            AtmUtils.assertLengthCheck(3, stringArray)
+        }
+    }
+}
+
