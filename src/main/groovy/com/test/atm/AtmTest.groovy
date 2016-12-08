0221cec759d2b86858e958a4963654a64004223f
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-08 12:30:12 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/AtmTest.groovy
+++ src/main/groovy/com/test/atm/AtmTest.groovy
@@ -4,8 +4,8 @@ package com.test.atm
  * Created by Test on 12/07/2016.
  */
 class AtmTest extends GroovyTestCase {
-    private Atm atm;
-    private Map<BankNote, Integer> atmTest = new TreeMap<>();
+    private Atm atm
+    private Map<BankNote, Integer> atmTest = new TreeMap<>()
 
     void setUp() {
         atm = new Atm()
