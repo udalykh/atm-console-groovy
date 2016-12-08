6bce8639f6c767e7d2898d7d75e6f4efc64b92c9
DUdalykh <dmitry.udalykh@yandex.ru>
2016-12-08 12:31:29 +0800

    Groovy refactoring

--- src/main/groovy/com/test/atm/BanknoteTest.groovy
+++ src/main/groovy/com/test/atm/BanknoteTest.groovy
@@ -4,7 +4,7 @@ package com.test.atm
  * Created by Test on 12/07/2016.
  */
 class BanknoteTest extends GroovyTestCase {
-    void testBanknoteToString() throws AtmStateException {
+    void testBanknoteToString() {
         BankNote testBanknote = new BankNote(Currency.USD, 100)
         String expected = 'USD 100'
         assertEquals(testBanknote.toString(), expected)
